9abc0288a254da0f70488397f25fbe483d4e90e9
Nestor Martinez <nnn3103m@gmail.com>
2021-06-06 16:42:07 -0600

    Recarga Claro y Tigo
    Se agrego las recargas claro y tigo

---
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_ii_l1_laboratorio1/onlygitupload.kt
@@ -0,0 +1,11 @@
+package hn.edu.ujcv.pdm_2021_ii_l1_laboratorio1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class onlygitupload : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onlygitupload)
+    }
+}
